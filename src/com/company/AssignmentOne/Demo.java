54262af0a4174025435855b85572de9b6b6f8c96
Mr-Ajinkya <virkud.ajinkya2248@gmail.com>
2020-05-27 17:00:56 +0530

    Ajinkya - Assignment 1 calling two methods from main class

---
+++ src/com/company/AssignmentOne/Demo.java
@@ -0,0 +1,13 @@
+package com.company.AssignmentOne;
+
+public class Demo {
+
+    public static void main(String[] args) {
+
+        Test test = new Test();
+        test.m1();
+        test.m2();
+
+    }
+
+}
---
+++ src/com/company/AssignmentOne/Test.java
@@ -0,0 +1,17 @@
+package com.company.AssignmentOne;
+
+public class Test {
+
+    int a = 10, b = 20;
+
+    void m1(){
+        System.out.println(a);
+        System.out.println(b);
+    }
+
+    void m2(){
+        System.out.println(a);
+        System.out.println(b);
+    }
+
+}
