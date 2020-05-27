1f7ba179d21c5e4935f4c84c6ff6be043abfd082
Mr-Ajinkya <virkud.ajinkya2248@gmail.com>
2020-05-27 17:29:46 +0530

    Ajinkya - Assignment 3 calling static variable from static methods

---
+++ src/com/company/assignmentthree/Demo.java
@@ -0,0 +1,19 @@
+package com.company.assignmentthree;
+
+public class Demo {
+
+    static int a = 100;
+    static int b = 200;
+
+    static void m1(){
+        System.out.println(Demo.a); // access static instance variables using Class name
+        System.out.println(Demo.b);
+    }
+
+
+    static void m2(){
+        System.out.println(Demo.a); // access static instance variables using Class name
+        System.out.println(Demo.b);
+    }
+
+}
---
+++ src/com/company/assignmentthree/Test.java
@@ -0,0 +1,10 @@
+package com.company.assignmentthree;
+
+public class Test {
+    public static void main(String[] args) {
+
+        Demo.m1(); //Access the static method by using class name
+        Demo.m2();
+
+    }
+}
