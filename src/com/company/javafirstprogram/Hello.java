fe499a22ab8dbef7e95f4d5ca856f41ab422afdf
Mr-Ajinkya <virkud.ajinkya2248@gmail.com>
2020-06-18 17:18:57 +0530

    Ajinkya - variables and expression java basic

--- src/com/company/javafirstprogram/Hello.java
+++ src/com/company/javafirstprogram/Hello.java
@@ -6,6 +6,16 @@ public class Hello {
 
         System.out.println("I'm Ajinkya. I love to do coding ");
 
+        int myFirstNumber = 5;
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println(myTotal);
+
+        int myLastOne = 1000 - myTotal;
+        System.out.println(myLastOne);
+
+
     }
 
 }
