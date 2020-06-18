abf37b6b45872f36b017ee383efc68d2512b7a61
Mr-Ajinkya <virkud.ajinkya2248@gmail.com>
2020-06-18 15:45:21 +0530

    Ajinkya - Print simple statement java first program

---
+++ src/com/company/javafirstprogram/Hello.java
@@ -0,0 +1,11 @@
+package com.company.javafirstprogram;
+
+public class Hello {
+
+    public static void main(String[] args) {
+
+        System.out.println("I'm Ajinkya. I love to do coding ");
+
+    }
+
+}
