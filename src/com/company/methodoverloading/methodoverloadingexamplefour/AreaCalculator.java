312367625ac5fde604932bd2f6e1638e4043ea60
Mr-Ajinkya <virkud.ajinkya2248@gmail.com>
2020-06-26 10:18:26 +0530

    Ajinkya - Calculate circle radius and rectangle area

--- src/com/company/methodoverloading/methodoverloadingexamplefour/AreaCalculator.java
+++ src/com/company/methodoverloading/methodoverloadingexamplefour/AreaCalculator.java
@@ -1,4 +1,33 @@
 package com.company.methodoverloading.methodoverloadingexamplefour;
 
+import java.io.Serializable;
+
 public class AreaCalculator {
+
+    public static void main(String[] args) {
+
+        double circleRadius = area(-1);
+        System.out.println(circleRadius);
+        
+        double rectangleArea = area(-1.0,4.0);
+        System.out.println(rectangleArea);
+
+    }
+
+    private static double area(double x,double y) {
+        if (x < 0 || y < 0){
+            return -1.0;
+        }else {
+            return x * y;
+        }
+    }
+
+    public static double area(double radius) {
+        if (radius < 0){
+            return -1.0;
+        }else{
+           return Math.PI * radius * radius;
+        }
+    }
+
 }
